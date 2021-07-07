0f974815be58a318fce1e75242eac4217013653f
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2021-07-08 01:53:43 +0800

    adjust build config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,18 @@ export default defineConfig({
             name: 'v-demi-request',
             fileName: 'index'
         },
+        rollupOptions: {
+            external: ['vue', '@vue/composition-api', 'vue-demi', 'vue2'],
+            output: {
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    vue: 'Vue',
+                    '@vue/composition-api': 'Vue2CompositionApi',
+                    vue2: 'Vue2',
+                    'vue-demi': 'VueDemi'
+                }
+            }
+        }
     },
     optimizeDeps: {
         exclude: ['vue-demi']
